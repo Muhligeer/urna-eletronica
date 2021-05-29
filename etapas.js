b0185237519f541276b4a0bd616eee5efd35e722
GUILHERME\PICHAU <guilherme.cruz@mirumagency.com>
2021-05-29 00:08:36 -0300

    urna

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '12345',
+        nome: 'Ze ruela',
+        partido: 'PSG',
+        fotos: [
+          {url: '12345.png', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: '54321',
+        nome: 'Batateiro do Rio',
+        partido: 'ABC',
+        fotos: [
+          {url: '54321.png', legenda: 'Vereador', }
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '11',
+        nome: 'Ze repolho',
+        partido: 'PSG',
+        vice: 'Dona Maria',
+        fotos: [
+          {url: '11.jpg', legenda: 'Prefeito'},
+          {url: '11_2.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: '21',
+        nome: 'Paulo do espetinho',
+        partido: 'POI',
+        vice: 'Seu Joao',
+        fotos: [
+          {url: '21.jpg', legenda: 'Prefeito'},
+          {url: '21_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      }
+    ]
+  }
+]
